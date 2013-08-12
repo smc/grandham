ead32000dd4d6a28e509b9e4740a184b74b427ca
Ershad K <ershad92@gmail.com>
2013-08-12 23:28:18 +0530

    Adding approve-edit method

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,5 +1,22 @@
 class Admin::ReviewsController < AdminController
+
+  before_filter :find_edit, except: [ :index ]
+
   def index
     @edits = current_language.edits
+  end
+
+  def approve
+    object = @edit.editable
+
+    object.update_attribute(@edit.field, @edit.new_value)
+
+    redirect_to language_admin_reviews_path(current_language)
+  end
+
+  private
+
+  def find_edit
+    @edit = current_language.edits.find(params[:id])
   end
 end
