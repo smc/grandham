903c9bf66dcc9c3e3e3e649c748a886d8bb4c71b
Ershad K <ershad92@gmail.com>
2013-08-13 00:25:45 +0530

    Adding external_link helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
       send "#{resource}_path"
     end
   end
+
+  def external_link(object)
+    path = "language_#{object.class.to_s.downcase}_path"
+
+    content_tag :span, class: 'external_link' do
+      link_to send(path, object.language, object) do
+        content_tag :i, class: 'icon-external-link' do
+        end
+      end
+    end
+  end
 end
