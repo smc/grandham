04923f2de0fd3c8e0e17a5870acff080136589fe
Ershad K <ershad92@gmail.com>
2013-09-06 09:26:21 +0530

    Redirect to previous page after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
+    session[:redirect_url] = request.url
     redirect_to new_user_session_path, :alert => "You are not authorized to access #{request.url}. Please sign in."
+  end
+
+  before_filter do |controller|
+    if user_signed_in? && (redirect_path = session.delete(:redirect_url))
+      redirect_to redirect_path
+    end
   end
 
   include EditHelper
