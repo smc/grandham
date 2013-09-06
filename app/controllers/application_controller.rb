1020be83c2ec1f5b852240e68323953ff3b356d2
Ershad K <ershad92@gmail.com>
2013-09-06 09:39:17 +0530

    Show proper alert messages on access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     session[:redirect_url] = request.url
-    redirect_to new_user_session_path, :alert => "You are not authorized to access #{request.url}. Please sign in."
+    redirect_to new_user_session_path, :alert => alert_message
   end
 
   before_filter do |controller|
@@ -15,6 +15,14 @@ class ApplicationController < ActionController::Base
   include EditHelper
 
   private
+
+  def alert_message
+    if request.path == '/books/new'
+      "Please sign in to add a new book"
+    elsif request.path =~ /.*\/books\/.*edit/
+      "Please sign in to edit the book '#{current_book.title}'"
+    end
+  end
 
   def current_language
     Language.where('short_code = ?', params[:language_id]).first
