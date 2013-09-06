412329ef4c181721f958f9c8ede78cdcac9382da
Ershad K <ershad92@gmail.com>
2013-09-06 08:35:38 +0530

    Show flash notice on access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, :alert => "You are not authorized to access #{request.url}. Please sign in."
+  end
+
   include EditHelper
 
   private
