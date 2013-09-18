872251d47e34c8799b766c6eb30d41b4b30f709b
Ershad K <ershad92@gmail.com>
2013-09-19 00:08:47 +0530

    Changing .empty? to .blank?

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,6 +1,6 @@
 module BooksHelper
   def formatted_name(book)
-    year = "(#{book.year})" unless book.year.empty?
+    year = "(#{book.year})" unless book.year.blank?
 
     "#{book.title} #{year}"
   end
