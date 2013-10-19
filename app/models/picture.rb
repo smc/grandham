856c111c4ffd1bb00ee1646bfbb862fe575b348f
Ershad K <ershad92@gmail.com>
2013-10-19 14:44:53 +0530

    Changing books listing

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   attr_accessible :image, :book_id
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, :default_url => "/book_cover_missing.png"
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>", small_thumb: "64x64>" }, :default_url => "/book_cover_missing.png"
 
   belongs_to :imageable, polymorphic: true
 end
