a2ea53e0ed94d9a5280f0e60c6619a6f7ca84ca6
Ershad K <ershad92@gmail.com>
2013-11-01 21:46:41 +0530

    Bootstrap 3 changes in Book views

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,15 +1,32 @@
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+
+    config.wrappers :bootstrap, tag: :div, class: "form-group", error_class: "has-error" do |b|
+
+    # Form extensions
     b.use :html5
     b.use :placeholder
+
+    # Form components
     b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+    b.wrapper tag: :div do |ba|
       ba.use :input
-      ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
-      ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+      ba.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+      ba.use :error, wrap_with: { tag: :span, class: "help-block text-danger" }
     end
   end
+
+
+  # config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+  #   b.use :html5
+  #   b.use :placeholder
+  #   b.use :label
+  #   b.wrapper :tag => 'div', :class => 'controls' do |ba|
+  #     ba.use :input
+  #     ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+  #     ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+  #   end
+  # end
 
   config.wrappers :prepend, :tag => 'div', :class => "control-group", :error_class => 'error' do |b|
     b.use :html5
