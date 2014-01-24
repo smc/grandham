b38efabd4446794717e37e0a5554171daee4a658
Ershad K <ershad92@gmail.com>
2014-01-24 23:29:28 +0530

    Adding basic tests for search pages

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,10 +2,36 @@ require 'spec_helper'
 
 describe SearchesController do
   describe "show" do
-    it "should return http_success" do
-      get :show
+    context "Books search" do
+      it "should return http_success" do
+        get :books
 
-      expect(response).to be_success
+        expect(response).to be_success
+      end
+    end
+
+    context "Authors search" do
+      it "should return http_success" do
+        get :authors
+
+        expect(response).to be_success
+      end
+    end
+
+    context "Libraries search" do
+      it "should return http_success" do
+        get :libraries
+
+        expect(response).to be_success
+      end
+    end
+
+    context "Publishers search" do
+      it "should return http_success" do
+        get :publishers
+
+        expect(response).to be_success
+      end
     end
 
     xit "should invoke Search.new" do
