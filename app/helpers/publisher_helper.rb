19f4ebd02f4480dec2af0e5028143e8c39988a9e
Ershad K <ershad92@gmail.com>
2014-01-26 22:45:25 +0530

    Removing a bunch of unused files

--- app/helpers/publisher_helper.rb
+++
@@ -1,9 +0,0 @@
-module PublisherHelper
-  def publisher_logo(publisher)
-    if publisher.logos.empty?
-      '/book_cover_missing.png'
-    else
-      publisher.logos.last.image.url(:thumb)
-    end
-  end
-end
