52ce813d4731dbe7caf00876aff0c2f62849666f
Ershad K <ershad92@gmail.com>
2014-12-15 00:25:47 +0530

    Updating edit functionality

--- app/helpers/edit_helper.rb
+++ app/helpers/edit_helper.rb
@@ -1,23 +1,24 @@
 module EditHelper
   def record_edit(object, object_params, book_id = nil)
+    object_params.keys.each do |field|
+      # Safeguarding the following .send call.
+      return unless object.class.new.attributes.keys.include?(field)
 
-    field = object_params.keys.first
+      old_value = object.send(field)
+      new_value = object_params[field]
 
-    # Safeguarding the following .send call.
-    return unless object.class.new.attributes.keys.include?(field)
+      unless old_value.to_s == new_value.to_s
+        if current_user.is_an_admin?
+          object.update_attributes object_params
+          state = 'approved'
+        else
+          flash[:notice] = 'Your edit(s) have been submitted for approval.'
+          state = 'open'
+        end
 
-    old_value = object.send(field)
-    new_value = object_params[field]
-
-    if current_user.is_an_admin?
-      object.update_attributes object_params
-      state = 'approved'
-    else
-      flash[:notice] = 'Your edit(s) have been submitted for approval.'
-      state = 'open'
+        object.edits.create old_value: old_value, new_value: new_value, user_id: current_user.id, field: field, language_id: object.language.id, book_id: book_id, state: state
+      end
     end
-
-    object.edits.create old_value: old_value, new_value: new_value, user_id: current_user.id, field: field, language_id: object.language.id, book_id: book_id, state: state
   end
 
   def author_path(object, param = nil)
