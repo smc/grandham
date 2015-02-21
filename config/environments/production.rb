3467aed9b49227d7a041e132ca8eca9775002142
Ershad K <ershad92@gmail.com>
2015-02-21 23:26:44 +0530

    Set default URL host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Grandham::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { host: 'grandham.org' }
+
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
