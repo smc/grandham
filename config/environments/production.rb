133a136568d074e495ed7b5e829ce22bca97d1bc
Ershad K <ershad92@gmail.com>
2015-02-21 23:08:34 +0530

    Configuring Mandril

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ Grandham::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => "USER_NAME",
+    :password  => "SECRET_KEY"
+  }
 end
