33c93327df62f6dc3233567568fda85ffa671806
Roshan Jossey <roshanjossey@gmail.com>
2016-02-09 12:01:32 +0000

    Add shoulda matchers, add validations and specs for book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe Book do
 
+  it{ should validate_presence_of(:title) }
+  it{ should validate_presence_of(:isbn) }
+
+  it{ should validate_numericality_of(:year) }
+
+  it{ should belong_to(:language) }
+
+  it{ should have_many(:authors) }
+  it{ should have_many(:authorships) }
+  it{ should have_many(:publications) }
+  it{ should have_many(:publishers) }
+  it{ should have_many(:availabilities) }
+  it{ should have_many(:libraries) }
+  it{ should have_many(:edits) }
+  it{ should have_many(:new_items) }
+  it{ should have_many(:covers) }
+
 end
