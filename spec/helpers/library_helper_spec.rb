a85bcc1a0941abd03b7e26ffaee25301995f2b67
vagrant <vagrant@precise64.(none)>
2016-02-09 06:56:32 +0000

    Remove unnecessary specs

--- spec/helpers/library_helper_spec.rb
+++ spec/helpers/library_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe LibraryHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe LibraryHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
