0ee11a8ef21ad1a46c23f5076e306d387d276e3f
Aboobacker MK <aboobackervyd@gmail.com>
2018-10-04 11:39:06 +0530

    Disable elastic search in test for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] = 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'capybara/rails'
 require 'capybara/rspec'
@@ -58,6 +58,20 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    # reindex models
+    # Book.reindex
+
+    # and disable callbacks
+    Searchkick.disable_callbacks
+  end
+
+  config.around(:each, search: true) do |example|
+    Searchkick.callbacks(true) do
+      example.run
+    end
+  end
 end
 require 'shoulda/matchers'
 
